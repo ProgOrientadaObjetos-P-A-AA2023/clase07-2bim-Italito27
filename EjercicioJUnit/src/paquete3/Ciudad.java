73288af9d6f2fd975147767f71f6bea9d47f3b6f
Italito27 <iilopez1@utpl.edu.ec>
2023-07-20 11:38:40 -0500

    iiii

--- EjercicioJUnit/src/paquete3/Ciudad.java
+++ EjercicioJUnit/src/paquete3/Ciudad.java
@@ -10,6 +10,29 @@ package paquete3;
  * @author reroes
  */
 public class Ciudad {
+    private String nombre;
+    private int poblacion;
     
+    public void establecerNombre(String nom){
+        nombre = nom;
+    }
     
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public void establecerPoblacion(int nom){
+        poblacion = nom;
+    }
+    
+    public int obtenerPoblacion(){
+        return poblacion;
+    }
+    @Override
+    public String toString() {
+        String cadena = String.format("Ciudad de Ecuador\n Nombre: %s\n\n\t"
+                + "Población: %d\n",
+         obtenerNombre(),obtenerPoblacion());
+        return cadena;
+    }
 }
